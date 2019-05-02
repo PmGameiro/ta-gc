d68db3684f20498d47c9476ffda10f6547af4697
PmGameiro <45496102+PmGameiro@users.noreply.github.com>
2019-05-02 22:59:59 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 12025994,
+    "boundingBox": {
+        "lx": 554031.01,
+        "ly": 4505753.56,
+        "lz": -19.27,
+        "ux": 554387.12,
+        "uy": 4506109.67,
+        "uz": 336.83999999998607
+    },
+    "tightBoundingBox": {
+        "lx": 554031.01,
+        "ly": 4505753.5600000009,
+        "lz": -19.27,
+        "ux": 554387.12,
+        "uy": 4506071.57,
+        "uz": 46.22
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.4672024250030519,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
